629765ef1be5342adedfd68be64bc2bebaf48841
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-03-24 12:57:33 +0100

    first commit

---
+++ database/migrations/2026_03_24_111019_create_trains_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code');
+            $table->unsignedTinyInteger('number_of_carriages');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
